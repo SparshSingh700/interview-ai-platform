9a9fe4c8901df319e9d2d40c7c58898a12116ed2
Sparsh Singh <sparshsingh700721@gmail.com>
2026-09-23 16:42:38 +0530

    implemented  user registration with password hashing

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel= require("../models/user.model.js");
-
+const jwt= require("jsonwebtoken")
+const bcrypt= require("bcrypt")
 /**
  * @name registerUserController
  * @description Controller to handle user registration
@@ -25,6 +26,28 @@ async function registerUserController(req, res) {
             return res.status(400).json({message: "User with this email already exists"});
         }     
     }
+
+    const hashedPassword= await bcrypt.hash(password, 10);
+    const user= await userModel.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    const token= jwt.sign(
+        {user: user._id, username: user.username},
+        process.env.JWT_SECRET,
+        { expiresIn: "4h"}
+    )
+    res.cookie("token", token)
+    res.status(201).json({
+        message: "User registered successfully",
+        user:{
+            id:user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
 }
 
 module.exports= {registerUserController}
