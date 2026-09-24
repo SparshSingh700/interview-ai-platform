2f1b37e5d9549f8093a601d136a662ab246c73cf
Sparsh Singh <sparshsingh700721@gmail.com>
2026-09-24 23:14:37 +0530

    enforce JWT token blacklist

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,16 +1,30 @@
 const jwt = require("jsonwebtoken");
+const tokenBlacklistModel = require("../models/blacklist.model.js");
 
-function authMiddleware(req, res, next){
-    const token= req.cookies.token;
-    if(!token){
-        return res.status(401).json({message: "Unauthorized access. Please login to continue."})
+async function authMiddleware(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized access. Please login to continue."
+        });
     }
-    try{
+
+    const isTokenBlackListed = await tokenBlacklistModel.findOne({ token });
+    if (isTokenBlackListed) {
+        return res.status(401).json({
+            message: "Unauthorized access. Please login to continue."
+        });
+    }
+
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user= decoded;
+        req.user = decoded;
         next();
-    }catch(err){
-        return res.status(401).json({message: "Invalid token"})
+    } 
+    catch (err) {
+        return res.status(401).json({
+            message: "Invalid token"
+        });
     }
 }
 
